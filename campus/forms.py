75b1fe2d54c010f556c1419c44fa3559f5e7b672
brocode <kelimbth@gmail.com>
2023-10-30 15:13:14 +0300

    create data forms

---
+++ campus/forms.py
@@ -0,0 +1,182 @@
+from .models import BookedUnit, Faculty, Feedback, Lecture, LectureHall, RegisteredUnit, School
+from django import forms
+
+class SchoolRegistrationForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'text', 'class': 'mb-0',
+        }),
+        help_text='Enter the name of the school',
+        label='School',
+    )
+
+    class Meta:
+        model = School
+        fields = ['name']
+
+class AddLectureHall(forms.ModelForm):
+    SELECT_FLOOR = (
+        (None, '-- Select floor --'),
+        ('GF', 'Ground floor'),
+        ('1', 'First floor'),
+        ('2', 'Second floor'),
+        ('3', 'Third floor'),
+        ('4', 'Fourth floor'),
+    )
+
+    academic_block = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'text', 'class': 'mb-0',
+        }),
+        help_text='On which academic block is lecture hall located?',
+        label='Academic block',    
+    )
+    hall_no = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'text', 'class': 'mb-0',
+        }),
+        help_text='Enter the hall number or name of the hall/room',
+        label='Lecture room/hall no.',
+    )
+    floor = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-0',
+        }),
+        choices=SELECT_FLOOR,
+        help_text='Enter floor number of the hall',
+        label='Floor',    
+    )
+
+    class Meta:
+        model = LectureHall
+        fields = ['academic_block', 'hall_no', 'seating_capacity', 'floor', 'rating']
+
+class StudentUnitsRegistrationForm(forms.ModelForm):
+
+    unit = forms.ChoiceField(widget=forms.SelectMultiple(attrs={
+            'type': 'select', 'class': 'mb-0',
+        }),
+        help_text='You can select multiple units.',
+        label='Units',    
+    )
+
+    class Meta:
+        model = RegisteredUnit
+        fields = ['unit']
+
+class LecturerUnitsBookingForm(forms.ModelForm):
+    # from Faculty QS only filter lecturers and add them in the dropdown menu.
+    lecturer_qs = Faculty.objects.filter(position__icontains='lecturer')
+
+    SELECT_LECTURER = (
+        (None, '-- Select lecturer --'),
+        [(lec, lec) for lec in lecturer_qs],
+    )
+    SELECT_COURSE_NAME = (
+        (None, '-- Select course name --'),
+
+    )
+    SELECT_YEAR_OF_STUDY = (
+        (None, '-- Select year of study --'),
+        ('1', 'First years (Freshers)'),
+        ('2', 'Second years (Sophomores)'),
+        ('3', 'Third years (Juniors)'),
+        ('4', 'Fourth years (Seniors)'),
+    )
+    SELECT_SEMESTER = (
+        (None, '-- Select course name --'),
+        ('1', 'Semester 1'),
+        ('2', 'Semester 2'),
+    )
+    
+    lecturer = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_LECTURER,
+    )
+    course_name = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_COURSE_NAME,
+    )
+    
+    year_of_study = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_YEAR_OF_STUDY,
+        label='Year of study',
+    )
+    semester = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_SEMESTER,
+    )
+
+    class Meta:
+        model = BookedUnit
+        fields = ['lecturer', 'course_name', 'year_of_study', 'semester']
+
+class LectureSchedulingForm(forms.ModelForm):
+    # get QS for units a lecturer will be teaching and display in dropdown menu
+    unit_qs = lambda request: BookedUnit.objects.filter(lecturer=request.user.faculty.staff)
+    SELECT_BOOKED_UNIT = (
+        (None, '-- Select one unit --'),
+        [(unit, unit) for unit in unit_qs]
+    )
+
+    unit_name = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        label='Unit',
+        choices=SELECT_BOOKED_UNIT,
+    )
+    lecture_date = forms.DateField(widget=forms.DateInput(attrs={
+            'type': 'date', 'class': 'mb-0',
+        }),
+        help_text='Schedule a date for this lecture',    
+    )
+    start_time = forms.TimeField(widget=forms.TimeInput(attrs={
+            'type': 'time', 'class': 'mb-0',
+        }),
+        help_text='At what time will this lecture begin?',
+        label='Schedule start time',
+    )
+    end_time = forms.TimeField(widget=forms.TimeInput(attrs={
+            'type': 'time', 'class': 'mb-0',
+        }),
+        help_text='At what time will this lecture end?',
+        label='Schedule end time',
+    )
+
+    class Meta:
+        model = Lecture
+        fields = ['unit_name', 'lecture_date', 'start_time', 'end_time']
+
+class FeedbackForm(forms.ModelForm):
+    SELECT_TYPE_COMPLAINT = (
+        (None, '-- Select type of complaint --'),
+        ('Burnt bulbs', 'Burnt bulbs/flourescent tubes'),
+        ('Dysfunctional sockets', 'Dysfunctional sockets'),
+        ('Environmental noise', 'Environmental noise'),
+        ('Naked electrical wires', 'Naked electrical wires')
+        ('Poor lighting', 'Poor lighting'),
+        ('Unfavorable temperature', 'Unfavorable temperature i.e. too cold/hot'),
+    )
+
+    complaint = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-0',
+        }),
+        choices=SELECT_TYPE_COMPLAINT,
+        label='Complaints', 
+    )
+    description = forms.CharField(widget=forms.Textarea(attrs={
+            'type': 'text', 'class': 'mb-0', 'placeholder': 'Provide more details about your complaints/approvals ...',
+        }),
+        help_text='What are your complaints or what you love about this lecture hall/room.',   
+    )
+    rate = forms.CharField(widget=forms.NumberInput(attrs={
+            'type': 'number', 'class': 'mb-0', 'min': 0, 'max': 5,
+        }),
+        help_text='How do you rate this hall?',
+        label='Rating',    
+    )
+
+    class Meta:
+        model = Feedback
+        fields = ['complaint', 'description', 'rate']
