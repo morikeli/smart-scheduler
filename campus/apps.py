9eb6f592a23cda3dd3fe48e35f5495314c25a465
brocode <kelimbth@gmail.com>
2023-10-30 12:27:13 +0300

    override ready()

--- campus/apps.py
+++ campus/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CampusConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'campus'
+
+    def ready(self):
+        import campus.signals
