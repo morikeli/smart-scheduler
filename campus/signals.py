d611e2bc115b43c2eb17ba64ba32ac45c182c930
brocode <kelimbth@gmail.com>
2023-11-02 15:56:41 +0300

    deleted ❌ NOTE: this signals have been transferred to accounts.signals

--- campus/signals.py
+++ campus/signals.py
@@ -1,4 +1,4 @@
-from .models import BookedUnit, Faculty, Feedback, Lecture, LectureHall, RegisteredUnit, School, Student
+from .models import BookedUnit, Feedback, Lecture, LectureHall, RegisteredUnit
 from django.db.models.signals import pre_save
 from django import dispatch
 import uuid
@@ -6,11 +6,6 @@ import uuid
 
 @dispatch.receiver(pre_save, sender=BookedUnit)
 def generate_booked_unitID(sender, instance, **kwargs):
-    if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
-
-@dispatch.receiver(pre_save, sender=Faculty)
-def generate_facultyID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid.uuid4()).replace('-', '')[:30]
 
@@ -31,15 +26,5 @@ def generate_lecture_hallID(sender, instance, **kwargs):
 
 @dispatch.receiver(pre_save, sender=RegisteredUnit)
 def generate_registered_unitID(sender, instance, **kwargs):
-    if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
-
-@dispatch.receiver(pre_save, sender=School)
-def generate_schoolID(sender, instance, **kwargs):
-    if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
-
-@dispatch.receiver(pre_save, sender=Student)
-def generate_studentID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid.uuid4()).replace('-', '')[:30]
