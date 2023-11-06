2e77efd471446e1f98b289f0fd22a0f914b50abc
brocode <kelimbth@gmail.com>
2023-11-06 21:58:10 +0300

    update configs. 🔧

--- src/settings.py
+++ src/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [env('WEB_DOMAIN'), '127.0.0.1']
+ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
 # Application definition
 
