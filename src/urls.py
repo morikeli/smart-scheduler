02c6cc276836945c27bb80b701672036957b26ac
brocode <kelimbth@gmail.com>
2023-11-06 21:57:02 +0300

    update urlpatterns

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.conf import settings
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('auth/', include('accounts.urls')),
     path('campus/u/', include('campus.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
