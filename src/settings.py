a7ca4137db6efd4206b2cec9d1314756b2acdc09
brocode <kelimbth@gmail.com>
2023-11-09 13:18:28 +0300

    set DEBUG to True

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
