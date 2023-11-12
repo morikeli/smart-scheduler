58ccde6a7668cdf075e0e330ec1a425a9711e97e
brocode <kelimbth@gmail.com>
2023-11-12 20:50:51 +0300

    create new table

--- campus/models.py
+++ campus/models.py
@@ -56,6 +56,19 @@ class Lecture(models.Model):
         ordering = ['lecturer', 'lecture_hall', '-date_scheduled']
         verbose_name_plural = 'Scheduled lectures'
 
+class Notification(models.Model):
+    id = models.CharField(max_length=30, primary_key=True, unique=True, editable=False)
+    message = models.CharField(max_length=100)
+    scheduled_lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.scheduled_lecture.unit_name}'
+    
+    class Meta:
+        ordering = ['-date_created', 'message']
+        verbose_name_plural = 'Notifications'
+
 class LectureHall(models.Model):
     """ This db table stores records of all available lecture halls in the entire institution. """
     id = models.CharField(max_length=30, primary_key=True, unique=True, editable=False)
