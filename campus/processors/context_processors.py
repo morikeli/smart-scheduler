566a27f8ffaa1dfa634de4804eaa6054b0615799
brocode <kelimbth@gmail.com>
2023-11-18 13:10:07 +0300

    add comments

--- campus/processors/context_processors.py
+++ campus/processors/context_processors.py
@@ -5,18 +5,19 @@ from accounts.models import User
 
 def user_notifications(request):
     """ This function is used to display user notifications in all templates. """
+    
     student_notifications = []
     lecturer_notifications = []
     total_lec_notifications = 0
     total_stud_notifications = 0
     
-    if request.user.is_anonymous is False:
+    if request.user.is_anonymous is False:  # check if user is anonymous
         try:
-            lecturer_notifications = Notification.objects.filter(scheduled_lecture__lecturer=request.user.faculty) if str(request.user) == str(request.user.faculty) else []
+            lecturer_notifications = Notification.objects.filter(scheduled_lecture__lecturer=request.user.faculty) if str(request.user) == str(request.user.faculty) else []    # if logged in user is lec/HOD return notifications else return empty list
             total_lec_notifications = lecturer_notifications.count() if len(lecturer_notifications) > 0 else total_stud_notifications
 
         except:
-            student_notifications = Notification.objects.filter(scheduled_lecture__student=request.user.student) if str(request.user) == str(request.user.student) else []
+            student_notifications = Notification.objects.filter(scheduled_lecture__student=request.user.student) if str(request.user) == str(request.user.student) else []    # if logged in user is student return notifications else return empty list
             total_stud_notifications = student_notifications.count() if len(lecturer_notifications) > 0 else total_lec_notifications        
             
     context = {
