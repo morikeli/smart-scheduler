ed701c70928e12d603c298806ae17130d6394ab9
brocode <kelimbth@gmail.com>
2023-11-18 13:07:10 +0300

    context_processor - this context_processor is used to display notifications to users, i.e. students & lecturers

---
+++ campus/processors/context_processors.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.decorators import user_passes_test
+from campus.models import Lecture, Notification
+from accounts.models import User
+
+
+def user_notifications(request):
+    """ This function is used to display user notifications in all templates. """
+    student_notifications = []
+    lecturer_notifications = []
+    total_lec_notifications = 0
+    total_stud_notifications = 0
+    
+    if request.user.is_anonymous is False:
+        try:
+            lecturer_notifications = Notification.objects.filter(scheduled_lecture__lecturer=request.user.faculty) if str(request.user) == str(request.user.faculty) else []
+            total_lec_notifications = lecturer_notifications.count() if len(lecturer_notifications) > 0 else total_stud_notifications
+
+        except:
+            student_notifications = Notification.objects.filter(scheduled_lecture__student=request.user.student) if str(request.user) == str(request.user.student) else []
+            total_stud_notifications = student_notifications.count() if len(lecturer_notifications) > 0 else total_lec_notifications        
+            
+    context = {
+        'student_notifications': student_notifications,
+        'lecturer_notifications': lecturer_notifications,
+        'TotalStudentNotifications': total_stud_notifications,
+        'TotalLecturerNotifications': total_lec_notifications,
+    }
+    return context
